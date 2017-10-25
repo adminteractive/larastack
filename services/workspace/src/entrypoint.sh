f302f8b86f9bc04c5c748bc01720fc5e785dbfd2
Martin Ant <antmartin01@gmail.com>
2017-10-25 22:43:15 +0300

    Add local user for workspace and php services

--- services/workspace/src/entrypoint.sh
+++ services/workspace/src/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+# Add local user.
+USER_ID=${LOCAL_USER_ID:-9001}
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m container
+
+# Keep container alive.
 /bin/ping localhost -i 5 > /dev/null
